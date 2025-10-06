70f1f5587e0049e4e1387c659bf753843facb7ea
gurkanucar <25080366+gurkanucar@users.noreply.github.com>
2025-10-06 13:58:06 +0300

    Create UpperCaseConverter.java

---
+++ src/main/java/toolyverse/io/toolyverse/infrastructure/util/UpperCaseConverter.java
@@ -0,0 +1,27 @@
+package toolyverse.io.toolyverse.infrastructure.util;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.Locale;
+import java.util.Optional;
+
+//  @Convert(converter = UpperCaseConverter.class)
+
+@Component
+@Converter
+public class UpperCaseConverter implements AttributeConverter<String, String> {
+  @Override
+  public String convertToDatabaseColumn(String attribute) {
+    return Optional.ofNullable(attribute)
+        .map(s -> s.toUpperCase(Locale.ROOT))
+        .orElse(null);
+  }
+  
+  @Override
+  public String convertToEntityAttribute(String dbData) {
+    return Optional.ofNullable(dbData)
+        .map(s -> s.toUpperCase(Locale.ROOT))
+        .orElse(null);
+  }
+}
